8fec63cc4be762ec7e5bb3ca9e156a05614b95b6
Joshua Julius <joshjulius1@gmail.com>
2021-05-05 18:32:58 -0500

    frontend setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar />
+      <ShoppingList />
+    </div>
+  );
+}
+
+export default App;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://joshjulius:josh@mern-shopping.z08ak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+};
